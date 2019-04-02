90ec002b186b41b4f1e76f202d08836d8e1015ab
jumarinr <31374735+jumarinr@users.noreply.github.com>
2019-04-01 23:54:20 -0500

    cambios finales

--- src/app.js
+++ src/app.js
@@ -3,13 +3,114 @@ var app = express();
 const path = require('path');
 const hbs = require('hbs');
 require('./helpers');
+const fs = require('fs');
 const listadoCursos = require('../private/cursos.json');
+const listadoEstudiantes = require('../private/usuarios.json');
+const listadoMatriculas = require('../private/matriculados.json');
+let direccionCursos  = "../trabajoNode/private/cursos.json";
+let direccionMatriculas  = "../trabajoNode/private/matriculados.json";
+let direccionEstudiantes  = "../trabajoNode/private/usuarios.json";
 const bodyParser = require('body-parser');
 
 app.set( 'view engine' , 'hbs');
 
 const  roles = ['aspirante', 'coordinador', 'visitante'];
+function crearCurso(nombre, id, descripcion, precio, modalidad, intensidad) {
+ console.log(listadoCursos);
+ let cursos = listadoCursos
+ const resultado = cursos.find( curso => curso.id === id)
+ if (resultado) {
+   return "no se creo el curso porque este id ya existe, por favor ingrese otra"
+ }else{
+   let materia =   { "nombre" : nombre,
+     "id" : id ,
+     "descripcion" : descripcion,
+     "valor": precio,
+     "estado" : "disponible",
+     "modalidad": modalidad ? modalidad : null,
+     "intensidad": intensidad }
+     cursos.push(materia)
+     fs.writeFile(direccion , JSON.stringify(cursos),'utf8', (err)=>{
+       if(err){
+         console.log(err)
+         return "no se creo el curso"
+       }
+       else{
+           console.log('se ha creado el archivo editado el archivo');
+           return "curso creado"
+         }
+     })
+ }
+}
 
+function inscribirACurso(id, idMateria) {
+ let matriculas = listadoMatriculas;
+ let busca;
+ const resultado = matriculas.find( matricula => {
+   if (matricula.id === id && matricula.cursos){
+     for (var i = 0; i < matricula.cursos.length; i++) {
+       if (matricula.cursos[i]==idMateria){
+        busca = true;
+       }
+     }
+   }
+ })
+ if (busca) {
+   return "el estudiante ya esta registrado en esta";
+ }else{
+   let estudiante = listadoEstudiantes.find( matricula => matricula.documento == id)
+   let dataEstudiante = matriculas.find( matricula => matricula.id == id)
+   if (!estudiante){
+     return "el estudiante no se encuentra registrado"
+   }else{
+     if (matriculas.indexOf(dataEstudiante) ){
+      matriculas.splice(matriculas.indexOf(dataEstudiante) , 1);
+       dataEstudiante.cursos.push(idMateria);
+    }else{
+      dataEstudiante = {
+        id: id,
+        materias: [idMateria]
+      }
+    }
+     matriculas.push(dataEstudiante)
+     fs.writeFile(direccionMatriculas , JSON.stringify(matriculas),'utf8', (err)=>{
+       if(err){
+         console.log(err)
+         return "no se añadio el curso"
+       }
+       else{
+           console.log('se ha inscrito al estudiante');
+           return "curso añadido"
+         }
+     })
+   }
+ }
+}
+
+function inscribirUsuario(nombre, id, correo, numero) {
+ let usuarios = listadoEstudiantes;
+ const resultado = usuarios.find( usuario => usuario.documento == id)
+ if (resultado) {
+   return "no se creo el usuario porque con este id ya existe, por favor ingrese otra"
+ }else{
+   let usuario =   { "nombre" : nombre,
+     "documento" : id ,
+     "correo" : correo,
+     "numero": numero,
+     "rol" : "aspirante"}
+     usuarios.push(usuario)
+     fs.writeFile(direccionEstudiantes , JSON.stringify(usuarios),'utf8', (err)=>{
+       if(err){
+         console.log(err)
+         return "no se creo el usuario"
+       }
+       else{
+           console.log('se ha creado el usuario');
+           return "curso creado"
+         }
+     })
+ }
+}
 const partials = path.join(__dirname, '../partials');
 hbs.registerPartials(partials);
 
@@ -26,33 +127,50 @@ app.use(bodyParser.urlencoded({extended: false}));
 //   });
 // })
 
-app.get('/inicio', (req, res)=>{
-  res.render('index'), {
+app.get('/verCursos', (req, res)=>{
+  res.render('verCursos'), {
     cursos: listadoCursos
   }
 })
-
-app.post('/calculos',(req, res)=>{
-  res.render('calculos', {
-    documento: parseInt(req.body.documento),
-    nombre: req.body.nombre,
-    correo: req.body.correo,
-    telefono: parseInt(req.body.telefono)
-  });
+app.get('/inscribirCursos', (req, res)=>{
+  res.render('registroCurso');
+})
+app.post('/inscribirCursos', (req, res)=>{
+  res.render('registroCursoPost', {
+    mensaje: crearCurso(req.body.nombre, req.body.id, req.body.descripcion, req.body.precio, req.body.modalidad, req.body.intensidad)
+  })
 })
 
+app.get('/inscribir', (req, res)=>{
+  res.render('inscribir');
+})
+app.post('/inscribir', (req, res)=>{
+  res.render('inscribirPost', {
+    mensaje: inscribirUsuario(req.body.nombre, req.body.documento, req.body.correo, req.body.telefono)
+  })
+})
 app.get('/registroCurso', (req, res)=>{
   res.render('registroCurso')
 })
 
+
 app.post('/subirCursos' , (req, res)=>{
   console.log(req.body)
 })
-
+app.get('/verInscritos', (req, res)=>{
+  res.render('verInscritos')
+})
+app.post('/verInscritos', (req, res)=>{
+  res.render('verInscritosPost', {
+    id: parseInt(req.body.id)
+  })
+})
+app.get('/inicio' , (req, res)=>{
+  res.render('inicio')
+})
 app.get('*', (req, res)=>{
   res.render('error');
 })
-
 app.listen(3000, ()=>{
   console.log("el servidor monto, loool :v. ingrese en su navegador localhost:3000");
 })
--- src/helpers.js
+++ src/helpers.js
@@ -1,64 +1,132 @@
 const hbs = require('hbs');
 const fs = require('fs');
 let curso = require('../private/cursos.json');
+let matriculados = require('../private/matriculados.json')
 
-hbs.registerHelper('obtenerPromedio', (nota1, nota2, nota3)=>{
-  return (nota1 + nota2 + nota3) / 3
-})
 
-hbs.registerHelper('listar', ()=>{
-  listaEstudiantes = require('./listado.json')
-  let texto = ' Lista de estudiantes <br> '
-  listaEstudiantes.forEach(estudiante => {
-    texto = `${texto} el nombre es ${estudiante.nombre} <br>
-    su nota en matematicas es ${estudiante.matematicas} <br>
-    su nota en ingles es ${estudiante.ingles} <br>
-    su nota en programacion es ${estudiante.programacion} <br>
-    `
-  })
-  return texto
-})
 
 hbs.registerHelper('mostrarAspi', ()=>{
-  let texto = "<table>\
-  <thead>\
-  <th>Nombre</th>\
-  <th>descripcion</th>\
-  <th>valor></th>\
-  </thead>";
 
-  curso.forEach(curso => {
-    if(curso.estado == 'disponible'){
-    texto = texto + "<tr>" +
-    '<td>'+ curso.nombre +
-    '</td>' +
-    '<td>'  + curso.descripcion + '</td>' +
-    '<td>'  + curso.valor + '</td>' +
-    '</tr>' ;
-  }
-  })
-  texto = texto + '</tbody></table>';
-  return texto
+
+let texto = "<table>\
+<thead>\
+<th>Nombre</th>\
+<th>descripcion</th>\
+<th>valor></th>\
+</thead>";
+let key = 0
+curso.forEach(curso => {
+  if (curso.estado == "disponible"){
+  key = key + 1
+
+  texto = texto + "<tr>" +
+  '<td>'+ `<div id="accordion">
+  <div class="card">
+  <div class="card-header" id="headingOne">
+  <h5 class="mb-0">
+  <button class="btn btn-link" data-toggle="collapse" data-target="#collapse${key}a" aria-expanded="true" aria-controls="collapse${key}a">
+  ${curso.nombre}
+  </button>
+  </h5>
+  </div>
+
+  <div id="collapse${key}a" class="collapse show" aria-labelledby="heading${key}a" data-parent="#accordion">
+  <div class="card-body">
+  <table>
+  <thead>\
+  <th>Descripcion</th>\
+  <th>Modalidad</th>\
+  <th>Intensidad Horaria></th>\
+  </thead>
+  <tr>
+  <td>${curso.descripcion}</td>
+  <td>${curso.modalidad ? curso.modalidad : '--'}</td>
+  <td>${curso.intensidad ? curso.intensidad : '--'}</td>
+  </tr>
+  </table
+  </div>
+  </div>
+  </div>
+  </div>` +
+  '</td>' +
+  '<td>'  + curso.descripcion + '</td>' +
+  '<td>'  + curso.valor + '</td>' +
+  '</tr>' ;
+}
+
+})
+texto = texto + '</tbody></table>';
+return texto;
 })
 hbs.registerHelper('mostrar', ()=>{
 
-  let texto = "<table>\
+
+let texto = "<table>\
+<thead>\
+<th>Nombre</th>\
+<th>descripcion</th>\
+<th>valor></th>\
+</thead>";
+let key = 0
+curso.forEach(curso => {
+  key = key + 1
+
+  texto = texto + "<tr>" +
+  '<td>'+ `<div id="accordion">
+  <div class="card">
+  <div class="card-header" id="headingOne">
+  <h5 class="mb-0">
+  <button class="btn btn-link" data-toggle="collapse" data-target="#collapse${key}" aria-expanded="true" aria-controls="collapse${key}">
+  ${curso.nombre}
+  </button>
+  </h5>
+  </div>
+
+  <div id="collapse${key}" class="collapse show" aria-labelledby="heading${key}" data-parent="#accordion">
+  <div class="card-body">
+  <table>
   <thead>\
-  <th>Nombre</th>\
-  <th>descripcion</th>\
-  <th>valor></th>\
-  </thead>";
+  <th>Descripcion</th>\
+  <th>Modalidad</th>\
+  <th>Intensidad Horaria></th>\
+  </thead>
+  <tr>
+  <td>${curso.descripcion}</td>
+  <td>${curso.modalidad ? curso.modalidad : '--'}</td>
+  <td>${curso.intensidad ? curso.intensidad : '--'}</td>
+  </tr>
+  </table
+  </div>
+  </div>
+  </div>
+  </div>` +
+  '</td>' +
+  '<td>'  + curso.descripcion + '</td>' +
+  '<td>'  + curso.valor + '</td>' +
+  '</tr>' ;
+
+})
+texto = texto + '</tbody></table>';
+return texto;
+})
+hbs.registerHelper('listarCursosDisponibles', ()=>{
+  let texto = "<select required> + <option disabled selected >seleccione un curso</option>"
 
   curso.forEach(curso => {
-
-    texto = texto + "<tr>" +
-    '<td>'+ curso.nombre +
-    '</td>' +
-    '<td>'  + curso.descripcion + '</td>' +
-    '<td>'  + curso.valor + '</td>' +
-    '</tr>' ;
-
+    if(curso.estado == 'disponible'){
+    texto = texto + `
+    '<option value="${curso.id}">${curso.nombre} </option>`
+  }
   })
-  texto = texto + '</tbody></table>';
+  texto = texto + '</select>';
+  return texto
+})
+hbs.registerHelper('listarMisCursos', (id)=>{
+  let texto;
+
+  matriculados.forEach(matricula => {
+    if(matricula.id == id){
+    texto = matricula.cursos;
+  }})
   return texto
 })
